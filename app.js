3ffc94bc88e28660b012404539779cca3be10f03
Robin Mooney <robinmooney.student@careerdevs.com>
2019-01-02 20:45:14 +0000

    added and removed files and directories using the addFile and addfileSync and mkdir and unlink commands in the app.js file

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
-var fs = require(`fs`);
+var fs = require('fs');
 
-fs.readFile(`readme.txt`, `utf8`, function(err, data){
-fs.writeFile(`writeMe.txt`, data);
+fs.unlink('./stuff/writeMe.txt', function(){
+    fs.rmdir('stuff');
 });
+
+
+
+
+//var fs = require(`fs`);
+//
+//fs.readFile(`readme.txt`, `utf8`, function(err, data){
+//fs.writeFile(`writeMe.txt`, data);
+//});
 
 
 
