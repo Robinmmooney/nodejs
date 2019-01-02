f1ab1ecbb15af2dc7072cf4008145547552976f1
Robin Mooney <robinmooney.student@careerdevs.com>
2019-01-02 19:26:37 +0000

    updated the app.js file to include using the event and util modules native to node.js and then using evenlisteners to call different peole by name and have them speak

--- app.js
+++ app.js
@@ -1,8 +1,37 @@
-var stuff = require("./stuff");
+var events = require('events');
+var util = require('util');
 
-console.log(stuff.counter(['Shaun', 'Crystal', 'Ryu']));
-console.log(stuff.adder(5,6));
-console.log(stuff.adder(stuff.pi,5));
+var Person = function(name){
+this.name = name;
+};
+
+util.inherits(Person, events.EventEmitter);
+
+var james = new Person('james');
+var mary = new Person('mary');
+var ryu = new Person('ryu');
+var people = [james, mary, ryu];
+
+people.forEach(function(person){
+    person.on('speak', function(mssg){
+        console.log(person.name + ' said: ' + mssg); 
+
+    });
+
+});
+
+james.emit('speak' , 'hey Dudes');
+ryu.emit('speak' , 'I want a curry');
+
+
+
+
+
+//var stuff = require("./stuff");
+//
+//console.log(stuff.counter(['Shaun', 'Crystal', 'Ryu']));
+//console.log(stuff.adder(5,6));
+//console.log(stuff.adder(stuff.pi,5));
 
 
 
